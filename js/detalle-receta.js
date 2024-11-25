219434d08a66940acb3db1b61db7a89c34b3cc90
fMmUd <mongiatfranco@udesa.edu.ar>
2024-11-24 21:27:14 -0300

    modifique detalle-receta js

--- js/detalle-receta.js
+++ js/detalle-receta.js
@@ -9,33 +9,20 @@ fetch (`https://dummyjson.com/recipes/${id}`)
     .then(function(response) {
         return response.json();
     })
-    .then(function(result) {
-        console.log(result);
+    .then(function(data) {
+        console.log(data);
 
-        let instrucciones = '';
-        for (let i = 0; i < result.instructions.length; i++) {
-            instrucciones += <li>${result.instructions[i]}</li>;
-        }
-        main.innerHTML =  `
-        <img src=${result.image} alt="">
-        <div>
-            <h1>${result.name}</h1>
-            <p>${result.cookTimeMinutes}</p>
-            <h3>Instrucciones</h3>
-            <ol>
-                ${instrucciones}
-            </ol>
-            <h3>Categorías</h3>
-            <ul>
-                <li>Pizza</li>
-                <li>Italian</li>
-            </ul>
-            <h3>Dificultad</h3>
-            <p>Fácil</p>
-        </div>
-        `
-        
-    })
-    .catch(function(error) {
-        console.error("Error encontrando la receta:", error);
-    }) 
+        let titulo = document.querySelector("#detalle-receta h1")
+        titulo.innerHTML = data.name;
+
+        let tiempo = document.querySelector("#detalle-receta p")
+        tiempo.innerHTML = `tiempo de coccion: ${data.cookTimeMinutes}`
+
+        let foto = document.querySelector("#detalle-receta img")
+        foto.src = data.image
+
+        let instrucciones = document.querySelector("#detalle-receta ol")
+        for(let i =0; i < data.instrucciones.length; i++){
+            instrucciones.innerHTML += `<li> ${data.instrucciones[i]} </li>`
+    }
+})
